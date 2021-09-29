e8b15b993f1115ce45fb064e26b6865260a315b4
Fernando Reis Guimarães <fernandobhz@gmail.com>
2021-09-28 22:14:10 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
+
+const utcServiceUrl = `https://app-current-utc-git-fernandobh.herokuapp.com`;
+
+function Agora() {
+  const [utc, setUtc] = useState(`loading`);
+
+  const atualiza = () => {
+    axios.get(utcServiceUrl).then(({data}) => setUtc(data));
+  };
+
+  useEffect(atualiza, []);
+
+  return (
+    <>
+      Agora é: '{utc}', <button onClick={atualiza}>atualizar</button>
+    </>
+  );
+}
+
+function Abc(props) {
+  return <pre>{JSON.stringify(props, null, 2)}</pre>;
+}
+
+const Xyz = ({ x, y, z }) => (
+  <div>
+    X: {x}
+    <br />
+    Y: {y}
+    <br />
+    Z: {z}
+    <br />
+  </div>
+);
+
+function Termostato({ initialValue = 30 }) {
+  const [value, setValue] = useState(initialValue);
+
+  return (
+    <>
+      Quanto é quente para você?
+      <button onClick={() => setValue(value - 1)}>( - )</button>
+      <input type="text" value={value} />
+      <button onClick={() => setValue(value + 1)}>( + )</button>
+      Você disse que {value} é quente para você!!!
+    </>
+  );
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Techseract</p>
+        <Agora />
+        <Abc a="1" b="2" c="3"></Abc>
+        <Xyz x="1" y="2" z="3"></Xyz>
+
+        <Termostato />
       </header>
     </div>
   );
